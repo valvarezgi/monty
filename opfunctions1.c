0512badf2efb71220df07e9df5c94ec59824a491
valvarezgi <vale.agiraldo@gmail.com>
2020-05-14 15:00:23 -0500

    Opcode nop, added

--- opfunctions1.c
+++ opfunctions1.c
@@ -16,3 +16,17 @@ void add(stack_t **stack, unsigned int line_number)
     (*stack)->next->n = (*stack)->next->n + (*stack)->n;
     pop(stack, line_number);
 }
+
+
+/**
+ * nop - nop
+ * @stack: stack
+ * @line_number: counter
+ */
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+    (void)stack;
+    (void)line_number;
+
+}
